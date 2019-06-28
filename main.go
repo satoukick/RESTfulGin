224987c8aaa4cbfa6aa4bc1cdc5a3a216901a069
cyw <weems1995@hotmail.com>
2019-06-28 17:42:35 +0800

    add CRUD requests

--- main.go
+++ main.go
@@ -12,15 +12,6 @@ import (
 
 var db *gorm.DB
 
-func init() {
-	var err error
-	db, err = gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=postgres password=uqee.com sslmode=false")
-	if err != nil {
-		logs.Logger.Error("can not connect to database")
-	}
-	db.AutoMigrate(&todoModel{})
-}
-
 type todoModel struct {
 	gorm.Model
 	Title     string `json:"title"`
@@ -33,21 +24,40 @@ type transformedTodo struct {
 	Completed bool   `json:"Completed"`
 }
 
+func init() {
+	var err error
+	db, err = gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=postgres password=uqee.com sslmode=disable")
+	if err != nil {
+		logs.Logger.Error(err)
+	}
+	db.AutoMigrate(&todoModel{})
+}
+
+//TODO : test with http requests, CI, code coverage
 func main() {
 	router := gin.Default()
 
 	v1 := router.Group("api/v1/todos")
 	{
 		v1.POST("/", createTodo)
-		// v1.GET("/", fetchAllTodo)
-		// v1.GET("/:id", fetchSingleTodo)
-		// v1.PUT("/:id", updateTodo)
-		// v1.DELETE("/:id", deleteTodo)
+		v1.GET("/", fetchAllTodo)
+		v1.GET("/:id", fetchSingleTodo)
+		v1.PUT("/:id", updateTodo)
+		v1.DELETE("/:id", deleteTodo)
 	}
 	router.Run()
 }
 
 func createTodo(c *gin.Context) {
+	title := c.PostForm("title")
+	if title == "" {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status":  http.StatusCreated,
+			"message": "title should not be empty.",
+		})
+		return
+	}
+
 	completed, _ := strconv.Atoi(c.PostForm("completed"))
 	todo := todoModel{
 		Title:     c.PostForm("title"),
@@ -58,5 +68,105 @@ func createTodo(c *gin.Context) {
 		"status":     http.StatusCreated,
 		"message":    "Todo item created successfully!",
 		"resourceId": todo.ID,
+	})
+}
+
+// fetcHallTodo fetches all records
+func fetchAllTodo(c *gin.Context) {
+	var todos []todoModel
+	var _todos []transformedTodo
+
+	db.Find(&todos)
+	if len(todos) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status":  http.StatusNotFound,
+			"message": "No todo found!",
+		})
+		return
+	}
+	for _, item := range todos {
+		var completed bool
+		if item.Completed == 1 {
+			completed = true
+		}
+		new := transformedTodo{
+			ID:        item.ID,
+			Title:     item.Title,
+			Completed: completed,
+		}
+		_todos = append(_todos, new)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"data":   _todos,
+	})
+}
+
+// fetchSingleTodo fetches single records using ID
+func fetchSingleTodo(c *gin.Context) {
+	todo := todoModel{}
+	todoID := c.Param("id")
+
+	db.First(&todo, todoID)
+
+	if todo.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status":  http.StatusNotFound,
+			"message": "todo id not found",
+		})
+		return
+	}
+
+	completed := todo.Completed == 1
+	trans := transformedTodo{
+		ID:        todo.ID,
+		Title:     todo.Title,
+		Completed: completed,
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status": http.StatusOK,
+		"data":   trans,
+	})
+}
+
+func updateTodo(c *gin.Context) {
+	var todo todoModel
+	todoID := c.Param("id")
+
+	db.First(&todo, todoID)
+
+	if todo.ID <= 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"status":  http.StatusOK,
+			"message": "No todo found!",
+		})
+		return
+	}
+
+	completed, _ := strconv.Atoi(c.PostForm("completed"))
+	title := c.PostForm("title")
+	db.Model(&todo).Updates(todoModel{Completed: completed, Title: title})
+	c.JSON(http.StatusOK, gin.H{
+		"status":  http.StatusOK,
+		"message": "Todo updated successfully!",
+	})
+}
+
+func deleteTodo(c *gin.Context) {
+	var todo todoModel
+	todoID := c.Param("id")
+
+	db.First(&todo, todoID)
+	if todo.ID == 0 {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  http.StatusNotFound,
+			"message": "No todo found!",
+		})
+		return
+	}
+	db.Delete(&todo)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  http.StatusOK,
+		"message": "Todo deleted successfully!",
 	})
 }
